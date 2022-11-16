348340dd547a1b62abb3d9ab1fc3de21bf327c10
ThatChiefGuy <peroperje@gmail.com>
2022-11-16 22:53:08 +0100

    added colisions

--- cursor.py
+++ cursor.py
@@ -6,14 +6,16 @@ class Cursor(pygame.sprite.Sprite):
         super().__init__()
         self.mixer = mixer
         self.image = pygame.image.load("cursor.png")
-        self.image = pygame.transform.scale(self.image, (100, 60))
+        self.image = pygame.transform.scale(self.image, (30, 30))
         self.image.set_colorkey(color)
         self.rect = self.image.get_rect()
         self.gun_shoot = pygame.mixer.Sound("GunShotSnglShotEx PE1097508.mp3")
+        self.collision_cursor = pygame.sprite.Sprite()
 
     def update(self, mouse_position):
         # moving the pointer
         self.rect.center = mouse_position
+        self.collision_cursor.center = mouse_position
 
     def shoot(self, cursor, target_group):
         self.gun_shoot.play()
--- targets.py
+++ targets.py
@@ -4,8 +4,8 @@ import pygame.sprite
 class Target(pygame.sprite.Sprite):
     def __init__(self, position_x, position_y):
         super().__init__()
-        self.size_x = 100
-        self.size_y = 100
+        self.size_x = 150
+        self.size_y = 150
         self.image = pygame.image.load("target.png")
         self.image = pygame.transform.scale(self.image, (self.size_x, self.size_y))
         self.image.set_colorkey((0, 0, 0))
@@ -13,11 +13,6 @@ class Target(pygame.sprite.Sprite):
         self.rect.center = position_x, position_y
         self.target_spawn_time = 0
         self.current_time = 0
-
-    def timer(self):
-        self.current_time = pygame.time.get_ticks()
-        if self.current_time - self.target_spawn_time > 2000:
-            self.target_spawn_time = self.current_time
 
 
 
